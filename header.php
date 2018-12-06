206affe7cd077afb9a6a8a760bf33e1ed3224dd6
gerocurkan <43069527+gerocurkan@users.noreply.github.com>
2018-12-06 12:38:38 -0300

    para copiar

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+  
   <title><?php echo $titulo ?></title>
 
   <!-- BOOTSTRAP -->
@@ -38,7 +38,7 @@
       <div class="nav-buscador">
         <input type="text" name="" value="" placeholder="Buscar en Jallalla!">
       </div>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto text-center">
           <li class="nav-item ">
             <a class="nav-link" href="timeline.php">
